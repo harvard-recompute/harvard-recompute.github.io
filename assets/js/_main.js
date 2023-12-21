6564ab290b04338b524c2ee7046443ccbb317099
victoriaono <victorialily13@gmail.com>
2023-12-21 11:17:23 -0500

    added about page

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -122,15 +122,15 @@ $(function() {
   });
 
   // Add anchors for headings
-  $('.page__content').find('h1, h2, h3, h4, h5, h6').each(function() {
-    var id = $(this).attr('id');
-    if (id) {
-      var anchor = document.createElement("a");
-      anchor.className = 'header-link';
-      anchor.href = '#' + id;
-      anchor.innerHTML = '<span class=\"sr-only\">Permalink</span><i class=\"fas fa-link\"></i>';
-      anchor.title = "Permalink";
-      $(this).append(anchor);
-    }
-  });
+  // $('.page__content').find('h1, h2, h3, h4, h5, h6').each(function() {
+  //   var id = $(this).attr('id');
+  //   if (id) {
+  //     var anchor = document.createElement("a");
+  //     anchor.className = 'header-link';
+  //     anchor.href = '#' + id;
+  //     anchor.innerHTML = '<span class=\"sr-only\">Permalink</span><i class=\"fas fa-link\"></i>';
+  //     anchor.title = "Permalink";
+  //     $(this).append(anchor);
+  //   }
+  // });
 });
